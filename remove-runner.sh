f56aa61c85ddf74763beae4796e3120b4516dd57
khanh <k3nkukhanh@gmail.com>
2023-06-02 06:28:47 +0000

    add file

---
+++ remove-runner.sh
@@ -0,0 +1,18 @@
+set -ex
+
+github_token=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
+for item in elofun-devops; do
+  page=0
+  while true; do
+    page=$(expr ${page} + 1)
+    runners=$(curl -s -u token:${github_token} "https://api.github.com/orgs/${item}/actions/runners?page=${page}")
+    if [ $(echo ${runners} | jq -r '.runners | length') == 0 ]; then
+      break
+    fi
+    for runner in $(echo ${runners} | jq -r '.runners[] | select(.status == "offline") | tojson'); do
+      echo ${runner} | jq -r .
+      runner_id=$(echo ${runner} | jq -r .id)
+      curl -s -u token:${github_token} -X DELETE "https://api.github.com/orgs/${item}/actions/runners/${runner_id}"
+    done
+  done
+done
